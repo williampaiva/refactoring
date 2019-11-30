0e5f68a8086acc8dd854502e559a906a1a9a7a45
William Paiva <willmatpaiv@yahoo.com.br>
2019-11-30 02:25:56 -0300

    update and rename ATL file responsible to refactor inner method and add new sample - public members with getters and setters

---
+++ PublicMemberWithSetterAndGetters/src/com/william/mo631/refactoring/sample/Person.java
@@ -0,0 +1,20 @@
+package com.william.mo631.refactoring.sample;
+
+public class Person {
+	public String name;
+	public String lastName;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
